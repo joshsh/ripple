e6b91caddfd00779073737b49e538eac74a5d6c3
Joshua Shinavier <josh@fortytwo.net>
2008-07-03 23:02:33 +0000

    RippleProperties now inherits java.util.Properties and has type-specific set methods.

--- ripple-base/src/main/java/net/fortytwo/ripple/RippleProperties.java
+++ ripple-base/src/main/java/net/fortytwo/ripple/RippleProperties.java
@@ -10,25 +10,24 @@
 package net.fortytwo.ripple;
 
 import java.util.Properties;
+import java.util.Date;
 import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-//import net.fortytwo.ripple.rdf.RdfUtils;
-/*import net.fortytwo.ripple.model.enums.ExpressionOrder;
-import net.fortytwo.ripple.model.enums.EvaluationStyle;
-import net.fortytwo.ripple.model.enums.EvaluationOrder;*/
-
-//import org.openrdf.rio.RDFFormat;
-
-public class RippleProperties
+public class RippleProperties extends Properties
 {
-	private Properties props;
-	
-	public RippleProperties( final Properties props )
-	{
-		this.props = props;
-	}
-	
-	public String getString( final String name ) throws RippleException
+    public RippleProperties( final Properties defaults )
+    {
+        super( defaults );
+    }
+
+    public RippleProperties()
+    {
+        super();
+    }
+    
+    public String getString( final String name ) throws RippleException
 	{
 		String value = getProperty( name, true );
 		return value;
@@ -49,8 +48,53 @@ public class RippleProperties
 		
 		return value.equals( "true" );
 	}
-	
-	public int getInt( final String name ) throws RippleException
+
+    public void setBoolean( final String name, final boolean value ) throws RippleException
+    {
+        setProperty( name, "" + value );
+    }
+
+    public double getDouble( final String name ) throws RippleException
+	{
+		String value = getProperty( name, true );
+
+		try
+		{
+			return ( new Double( value ) ).doubleValue();
+		}
+
+		catch ( java.lang.NumberFormatException e )
+		{
+			throw new RippleException( e );
+		}
+	}
+
+    public void setDouble( final String name, final double value ) throws RippleException
+    {
+        setProperty( name, "" + value );
+    }
+
+    public float getFloat( final String name ) throws RippleException
+	{
+		String value = getProperty( name, true );
+
+		try
+		{
+			return ( new Float( value ) ).floatValue();
+		}
+
+		catch ( java.lang.NumberFormatException e )
+		{
+			throw new RippleException( e );
+		}
+	}
+
+    public void setFloat( final String name, final float value ) throws RippleException
+    {
+        setProperty( name, "" + value );
+    }
+
+    public int getInt( final String name ) throws RippleException
 	{
 		String value = getProperty( name, true );
 	
@@ -64,8 +108,13 @@ public class RippleProperties
 			throw new RippleException( e );
 		}
 	}
-	
-	public long getLong( final String name ) throws RippleException
+
+    public void setInt( final String name, final int value ) throws RippleException
+    {
+        setProperty( name, "" + value );
+    }
+
+    public long getLong( final String name ) throws RippleException
 	{
 		String value = getProperty( name, true );
 		
@@ -80,11 +129,41 @@ public class RippleProperties
 		}
 	}
 
+    public void setLong( final String name, final long value ) throws RippleException
+    {
+        setProperty( name, "" + value );
+    }
+
+    public URI getURI( final String name ) throws RippleException
+    {
+        String value = getProperty( name, true );
+
+        try
+        {
+            return new URI( value );
+        }
+
+        catch ( URISyntaxException e )
+        {
+            throw new RippleException( e );
+        }
+    }
+
+    public void setURI( final String name, final URI value ) throws RippleException
+    {
+        setProperty( name, "" + value );
+    }
+
     public File getFile( final String name ) throws RippleException
     {
         String value = getProperty( name, true );
         
         return new File( value );
+    }
+
+    public void setFile( final String name, final File value ) throws RippleException
+    {
+        setProperty( name, "" + value.getAbsolutePath() );
     }
 
     public File getFile( final String name, final File defaultValue ) throws RippleException
@@ -96,9 +175,20 @@ public class RippleProperties
                 : new File( value );
     }
 
+    public Date getDate( final String name ) throws RippleException
+    {
+        long millis = getLong( name );
+        return new Date( millis );
+    }
+
+    public void setDate( final String name, final Date value ) throws RippleException
+    {
+        setProperty( name, "" + value.getTime() );
+    }
+
     private String getProperty( final String name, final boolean required ) throws RippleException
 	{
-		String s = props.getProperty( name );
+		String s = getProperty( name );
 		
 		if ( null == s && required )
 		{
