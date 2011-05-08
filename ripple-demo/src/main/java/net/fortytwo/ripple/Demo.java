824619b81adf9a545fab720e61f161f9ee6b5dd8
Joshua Shinavier <josh@fortytwo.net>
2011-05-08 23:00:15 +0800

    Cleaned up formatting of the Demo app.

--- ripple-demo/src/main/java/net/fortytwo/ripple/Demo.java
+++ ripple-demo/src/main/java/net/fortytwo/ripple/Demo.java
@@ -30,136 +30,117 @@ import java.util.Properties;
 /**
  * Demo application.
  */
-public final class Demo
-{
-	private Demo()
-	{
-	}
+public final class Demo {
+    private Demo() {
+    }
 
-    public static void demo( final InputStream in,
-					 		final PrintStream out,
-							final PrintStream err )
-		throws RippleException
-	{
-//net.fortytwo.ripple.tools.SitemapsUtils.test();
-		// Create a Sesame triple store.
+    public static void demo(final InputStream in,
+                            final PrintStream out,
+                            final PrintStream err)
+            throws RippleException {
+        // Create a Sesame triple store.
         URIMap uriMap = new URIMap();
         SailConfiguration sailConfig = new SailConfiguration(uriMap);
         sailConfig.initialize();
         Sail sail = sailConfig.getSail();
 
         // Attach a Ripple model to the repository.
-		Model model = new SesameModel( sail, Ripple.class.getResource("libraries.txt"));
+        Model model = new SesameModel(sail, Ripple.class.getResource("libraries.txt"));
 
-		// Attach a query engine to the model.
+        // Attach a query engine to the model.
         StackEvaluator evaluator = new LazyStackEvaluator();
-//        StackEvaluator evaluator = new LazyDebugEvaluator();
-		QueryEngine qe
-			= new QueryEngine( model, evaluator, out, err );
+        QueryEngine qe
+                = new QueryEngine(model, evaluator, out, err);
 
-		// Attach an interpreter to the query engine and let it query from
-		// standard input.
-		CommandLineInterface r = new CommandLineInterface( qe, in );
-		r.run();
+        // Attach an interpreter to the query engine and let it query from
+        // standard input.
+        CommandLineInterface r = new CommandLineInterface(qe, in);
+        r.run();
 
         // Shut down.
         model.shutDown();
         sailConfig.shutDown();
-	}
+    }
 
-	private static void printUsage()
-	{
-		System.out.println( "Usage:  ripple [options] [configuration file]" );
-		System.out.println( "Options:\n"
-			+ "  -h           Print this help and exit\n"
-			+ "  -q           Suppress normal output\n"
-			+ "  -v           Print version information and exit" );
-		System.out.println( "For more information, please see:\n"
-			+ "  <URL:http://ripple.fortytwo.net/>." );
-	}
+    private static void printUsage() {
+        System.out.println("Usage:  ripple [options] [configuration file]");
+        System.out.println("Options:\n"
+                + "  -h           Print this help and exit\n"
+                + "  -q           Suppress normal output\n"
+                + "  -v           Print version information and exit");
+        System.out.println("For more information, please see:\n"
+                + "  <URL:http://ripple.fortytwo.net/>.");
+    }
 
-	private static void printVersion()
-	{
-		System.out.println( Ripple.getName() + " " + Ripple.getVersion() );
+    private static void printVersion() {
+        System.out.println(Ripple.getName() + " " + Ripple.getVersion());
 
-		// Would be nice: list of libraries
-	}
+        // Would be nice: list of libraries
+    }
 
-	public static void main( final String [] args )
-	{
-		// Default values.
-		boolean quiet = false, showVersion = false, showHelp = false;
-		File configFile = null;
+    public static void main(final String[] args) {
+        // Default values.
+        boolean quiet = false, showVersion = false, showHelp = false;
+        File configFile = null;
 
-		// Long options are available but are not advertised.
-		LongOpt [] longOptions = {
-			new LongOpt( "help", LongOpt.NO_ARGUMENT, null, 'h' ),
-			new LongOpt( "quiet", LongOpt.NO_ARGUMENT, null, 'q' ),
-			new LongOpt( "version", LongOpt.NO_ARGUMENT, null, 'v' ) };
+        // Long options are available but are not advertised.
+        LongOpt[] longOptions = {
+                new LongOpt("help", LongOpt.NO_ARGUMENT, null, 'h'),
+                new LongOpt("quiet", LongOpt.NO_ARGUMENT, null, 'q'),
+                new LongOpt("version", LongOpt.NO_ARGUMENT, null, 'v')};
 
-		Getopt g = new Getopt( Ripple.getName(), args, "hqv", longOptions );
-		int c;
-		while ( ( c = g.getopt() ) != -1 )
-		{
-			switch( c )
-			{
-				case 'h':
-				case 0:
-					showHelp = true;
-					break;
-				case 'q':
-				case 1:
-					quiet = true;
-					break;
-				case 'v':
-				case 2:
-					showVersion = true;
-					break;
-				case '?':
-					 // Note: getopt() already printed an error
-					printUsage();
-					System.exit( 1 );
-					break;
-				default:
-					System.err.print("getopt() returned " + c + "\n");
-			}
-		}
+        Getopt g = new Getopt(Ripple.getName(), args, "hqv", longOptions);
+        int c;
+        while ((c = g.getopt()) != -1) {
+            switch (c) {
+                case 'h':
+                case 0:
+                    showHelp = true;
+                    break;
+                case 'q':
+                case 1:
+                    quiet = true;
+                    break;
+                case 'v':
+                case 2:
+                    showVersion = true;
+                    break;
+                case '?':
+                    // Note: getopt() already printed an error
+                    printUsage();
+                    System.exit(1);
+                    break;
+                default:
+                    System.err.print("getopt() returned " + c + "\n");
+            }
+        }
 
-		int i = g.getOptind();
-		if ( i < args.length )
-		{
-			// Too many non-option arguments.
-			if ( args.length - i > 1 )
-			{
-				printUsage();
-				System.exit( 1 );
-			}
+        int i = g.getOptind();
+        if (i < args.length) {
+            // Too many non-option arguments.
+            if (args.length - i > 1) {
+                printUsage();
+                System.exit(1);
+            }
 
-			configFile = new File( args[i] );
-		}
+            configFile = new File(args[i]);
+        }
 
-		if ( showHelp )
-		{
-			printUsage();
-			System.exit( 0 );
-		}
+        if (showHelp) {
+            printUsage();
+            System.exit(0);
+        }
 
-		if ( showVersion )
-		{
-			printVersion();
-			System.exit( 0 );
-		}
+        if (showVersion) {
+            printVersion();
+            System.exit(0);
+        }
 
-		try
-		{
-			// Load Ripple configuration.
-			if ( null == configFile )
-			{
-				Ripple.initialize();
-			}
-			
-			else
-			{
+        try {
+            // Load Ripple configuration.
+            if (null == configFile) {
+                Ripple.initialize();
+            } else {
                 Properties p = new Properties();
 
                 try {
@@ -168,37 +149,30 @@ public final class Demo
                     throw new RippleException(e);
                 }
 
-                Ripple.initialize( p );
-			}
-		}
+                Ripple.initialize(p);
+            }
+        } catch (RippleException e) {
+            System.err.println("Initialization error: " + e);
+            e.logError();
+            System.exit(1);
+        }
 
-		catch ( RippleException e )
-		{
-			System.err.println( "Initialization error: " + e );
-			e.logError();
-			System.exit( 1 );
-		}
-		
-		Ripple.setQuiet( quiet );
+        Ripple.setQuiet(quiet);
 // System.out.println( "quiet = " + quiet );
 // System.out.println( "showVersion = " + showVersion );
 // System.out.println( "format = " + format );
 // System.out.println( "store = " + store );
 
-		try
-		{
-			demo( System.in, System.out, System.err );
-		}
+        try {
+            demo(System.in, System.out, System.err);
+        } catch (RippleException e) {
+            System.out.println("Exited with error: " + e);
+            e.logError();
+            System.exit(1);
+        }
 
-		catch ( RippleException e )
-		{
-			System.out.println( "Exited with error: " + e );
-			e.logError();
-			System.exit( 1 );
-		}
-
-		// Exit despite any remaining active threads.
-		System.exit( 0 );
-	}
+        // Exit despite any remaining active threads.
+        System.exit(0);
+    }
 }
 
